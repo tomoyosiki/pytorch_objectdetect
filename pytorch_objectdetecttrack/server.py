289bd5e84860fc46fe1098f58a446e94065cf67a
tomoyosiki <tomoyosiki@gmail.com>
2021-05-31 23:58:46 -0400

    Add code for debuging access to restful api

--- pytorch_objectdetecttrack/server.py
+++ pytorch_objectdetecttrack/server.py
@@ -1,12 +1,62 @@
 import object_detection
 import os
 from flask import Flask, request, Response, render_template
+import json
+import requests
+from pager import Pager
 
 app = Flask(__name__)
+def getAPI(type):
+    if type == 'get_sensor_data':
+        return lambda n: "http://127.0.0.1:5000/data/sensors/{}".format(n)
+    if type == 'get_sensor_list':
+        return lambda n: "http://127.0.0.1:5000/sensors/{}".format(n)
+
+def get_api_return(_api):
+    print("try to get API: {}".format(_api))
+    resp = requests.get(_api)
+    if resp.status_code == 200:        
+        contents = str(resp.content)[2:-3]        
+        return json.loads(contents)
+    return None
+
+get_available_camera_api = getAPI('get_sensor_list')
+get_data_api = getAPI('get_sensor_data')
+
+cameras_info = get_api_return(get_available_camera_api('camera'))
+cameras_name = []
+for camera in cameras_info['data']:
+    cameras_name.append(camera['sensor_name'])
+print("available sensors", cameras_name)
+
+if len(cameras_name) > 0:
+    select_camera = cameras_name[0]
+data_list = get_api_return(get_data_api(select_camera))['data']
+data_number = len(data_list)
+pager = Pager(data_number)
 
 @app.route('/')
 def index():
     return Response('Tensor Flow object detection')
+
+@app.route('/<int:ind>/')
+def image_view(ind=None):
+    if ind >= data_number:
+        return render_template("404.html"), 404
+    else:
+        pager.current = ind
+        local_name = data_list[ind]['token'] + '.jpg'
+        image_origin_path = data_list[ind]['path']
+        PATH_TO_TEST_IMAGES_DIR = 'static/images'  # cwh
+        image = os.path.join(PATH_TO_TEST_IMAGES_DIR, local_name)
+        print("origin : {}, local : {}".format(image_origin_path, image))
+        if local_name not in os.listdir(PATH_TO_TEST_IMAGES_DIR):
+            os.system('cp {} {}'.format(image_origin_path, image))
+            return render_template('imageview.html', index=ind, pager=pager, data={'name':image.replace(".jpg", "-det.jpg")})
+        else:
+            return render_template('imageview.html', index=ind, pager=pager, data={'name':image.replace(".jpg", "-det.jpg")})
+        
+
 
 @app.route('/test')
 def test():
